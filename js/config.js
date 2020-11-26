ea143da88b60ce364478b059f7cbe10f92164470
Shubham-Pachade <58845567+Shubham-Pachade@users.noreply.github.com>
2020-11-26 18:57:35 +0100

    Initialized

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_rv9VEangZ', // e.g. us-east-2_uXboG5pAb
+        region: 'eu-central-1', // e.g. us-east-2
+		clientId: '2hsdf82jl5ghudckmp50acvnbn' //is this used anywhere?
+    },
+};
+
